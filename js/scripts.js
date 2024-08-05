45b57ab1dea48cafc3104469c00c6391d98e3120
Gabriel <0088218@aluno.sertao.ifrs.edu.br>
2024-08-05 10:16:55 -0300

    Commit inicial

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+document.querySelector("#login-button")?.addEventListener('click', () => {
+    window.location.href = 'menu.html';
+});
+
+document.querySelector("#register-button")?.addEventListener('click', () => {
+    alert('Usuário Cadastrado com SUCESSO!');
+});
